ffd2433de63afd5541d91a83c3d9c5f084326870
hopperW12 <wolverinetomas@gmail.com>
2025-10-09 20:33:36 +0200

    Add article controller & service with simple functionality

---
+++ Newspoint.Application/Services/ArticleService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newspoint.Domain.Entities;
+using Newspoint.Infrastructure.Database;
+
+namespace Newspoint.Application.Services;
+
+public interface IArticleService : IService
+{
+    Task<ICollection<Article>> GetAll();
+}
+public class ArticleService : IArticleService
+{
+    private readonly DataDbContext _dataDbContext;
+    
+    public ArticleService(DataDbContext dataDbContext)
+    {
+        _dataDbContext = dataDbContext;
+    }
+
+    public async Task<ICollection<Article>> GetAll()
+    {
+        var articles = _dataDbContext.Articles.ToListAsync();
+        return await articles;
+    }
+}
---
+++ Newspoint.Server/Controllers/ArticleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newspoint.Application.Services;
+using Newspoint.Domain.Entities;
+
+namespace Newspoint.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ArticleController : ControllerBase
+{
+    private readonly IArticleService _articleService;
+
+    public ArticleController(IArticleService articleService)
+    {
+        _articleService = articleService;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Article>> GetAll()
+    {
+        return await _articleService.GetAll();
+    }
+}
