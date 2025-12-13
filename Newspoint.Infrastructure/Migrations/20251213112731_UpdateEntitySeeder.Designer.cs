f8d685c09b65f1100bc164859f5ef3751bbe4575
hopperW12 <wolverinetomas@gmail.com>
2025-12-13 12:48:19 +0100

    Add UpdateEntitySeeder (Migration)

--- Newspoint.Infrastructure/Migrations/20251213112731_UpdateEntitySeeder.Designer.cs
+++ Newspoint.Infrastructure/Migrations/20251213112731_UpdateEntitySeeder.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Newspoint.Infrastructure.Database;
 
@@ -11,9 +12,11 @@ using Newspoint.Infrastructure.Database;
 namespace Newspoint.Infrastructure.Migrations
 {
     [DbContext(typeof(DataDbContext))]
-    partial class DataDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251213112731_UpdateEntitySeeder")]
+    partial class UpdateEntitySeeder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Newspoint.Infrastructure/Migrations/20251213112731_UpdateEntitySeeder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Newspoint.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntitySeeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "LastName", "Password", "RegisteredAt", "Role" },
+                values: new object[] { 2, "redaktor@newspoint.com", "redaktor", "redaktor", "d437337e6abc848ac36eba2294ca57cb54c052d92ab7ad72071a7ab086761964", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec");
+        }
+    }
+}
